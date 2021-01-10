4df756dd5fef678775c3d0f696b432bbcaae9e64
unknown <alissonjosef@gmail.com>
2021-01-10 16:08:17 -0300

    javascript

---
+++ js/main.js
@@ -0,0 +1,113 @@
+
+function botao (){
+    document.getElementById("agredecimento").innerHTML = "Obrigado por clicar";
+    //console.log(document.getElementById("agredecimento"));
+    //alert("Obrigado por clicar");
+}
+function redirecionar(){
+    window.open("https://www.google.com.br");
+    //window.location.href = "https://www.google.com.br";
+}
+
+function trocar(elemento){
+    elemento.innerHTML = "Obrigado por passar o mouse";
+   // document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    //alert("Trocar Texto");
+}
+
+function voltar(elemento){
+    elemento.innerHTML = "Passa o mouse aqui";
+    //document.getElementById("mousemove").innerHTML = "Passar o mouse";
+}
+
+function load(){
+    alert("pagina carregada");
+}
+
+/* function soma (n1, n2){
+    return n1 + n2;
+} */
+
+/* function validaIdade(idade){
+    var validar;
+    if(idade >= 18){
+        validar= true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual a sua idade ");
+console.log(validaIdade(idade)); */
+
+/* function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome,novo_nome)
+} */
+
+//alert(soma(5,10));
+//alert(setReplace("Brasil","USA","Japão"));
+
+/* var d = new Date();
+alert(d.getMinutes());
+alert(d.getHours());
+alert(d.getMonth());
+alert(d.getSeconds()); */
+
+/* var count;
+for(count=0; count<=5; count++){
+    console.log(count);
+    alert(count);
+}; */
+
+/* var count = 0;
+while ( count < 5){
+    console.log(count);
+    count = count + 1;
+}; */
+
+
+/* var idade = prompt("Qual a sua idade");
+//var idade = 18;
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+}; */
+
+/* var fruta = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"verde"} ]
+console.log(fruta);
+alert(fruta[1].nome);
+ */
+/* var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta);
+alert(frutas.cor); */
+
+/* var lista = ["maça", "pêra", "laranja"];
+lista.push("uva"); */
+//lista.pop();
+//console.log(lista[2]);
+//alert(lista[2]);
+//console.log(lista.reverse(());
+//console.log(lista.length);
+//console.log(lista.toString());
+//console.log(lista.join(" | "));
+
+
+
+/* 
+var nome = "Alisson Silva";
+var idade = 29;
+var idade2 = 10;
+var frase = "Brasil e o melhor time do mundo"
+//alert(nome + " tem " + idade + " anos");
+
+//alert(idade + idade2)
+
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase);
+console.log(frase.replace("Brasil", "França"));
+console.log(frase.toLocaleUpperCase());
+console.log(frase.toLowerCase());
+console.log(idade * idade2) */
